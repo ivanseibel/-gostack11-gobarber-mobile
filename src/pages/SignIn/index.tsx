4e4feca2bee073f32f4e074789389c7cd4994741
Ivan Seibel <ivanseibel@gmail.com>
2020-07-03 16:24:54 -0300

    Add logo to SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
 
 import * as SC from './styles';
 
 const SignIn: React.FC = () => {
-  return <SC.Container />;
+  return (
+    <SC.Container>
+      <Image source={logoImg} />
+    </SC.Container>
+  );
 };
 
 export default SignIn;
