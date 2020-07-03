3b1cc267c026f6d229b3e391f0294ec12979009d
Ivan Seibel <ivanseibel@gmail.com>
2020-07-03 15:48:42 -0300

    Create basic routes with react-navigation

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as SC from './styles';
+
+const SignIn: React.FC = () => {
+  return <SC.Container />;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as SC from './styles';
+
+const SignUp: React.FC = () => {
+  return <SC.Container />;
+};
+
+export default SignUp;
