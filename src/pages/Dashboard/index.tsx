ec5b3b8c2c874cdaddb101ff92a25b8382e5d639
Ivan Seibel <ivanseibel@gmail.com>
2020-07-24 17:17:55 -0300

    Get providers list.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useCallback, useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import * as SC from './styles';
+import api from '../../services/api';
+
+export interface IProvider {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<IProvider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    signOut();
+    // navigate('Profile');
+  }, [navigate]);
+
+  useEffect(() => {
+    try {
+      console.log(api.defaults.headers.authorization);
+
+      api.get('providers').then(response => {
+        console.log(response.data);
+        setProviders(response.data);
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, []);
 
   return (
-    <Container style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Logoff" onPress={signOut} />
-    </Container>
+    <SC.Container>
+      <SC.Header>
+        <SC.HeaderTitle>
+          {`Welcome,\n`}
+          <SC.UserName>{user.name}</SC.UserName>
+        </SC.HeaderTitle>
+
+        <SC.ProfileButton onPress={navigateToProfile}>
+          <SC.UserAvatar source={{ uri: user.avatar_url }} />
+        </SC.ProfileButton>
+      </SC.Header>
+
+      <SC.ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <SC.UserName>{item.name}</SC.UserName>}
+      />
+    </SC.Container>
   );
 };
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,46 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { Platform, FlatList } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+import { Provider } from 'react';
+import { IProvider } from './index';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px 24px 24px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  ${Platform.OS === 'ios' &&
+  css`
+    padding-top: ${getStatusBarHeight()}px;
+  `}
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)``;
