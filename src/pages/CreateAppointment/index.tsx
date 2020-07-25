90a9fc02ddc614ceb88f9efd53d533c17a565f0e
Ivan Seibel <ivanseibel@gmail.com>
2020-07-25 12:56:15 -0300

    Implement basic structure and navigation.

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useRoute, useNavigation } from '@react-navigation/native';
+import * as SC from './styles';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+interface IRouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  const { signOut } = useAuth();
+  const route = useRoute();
+  const { providerId } = route.params as IRouteParams;
+  const { user } = useAuth();
+  const navigation = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
 
   return (
-    <Container style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Logoff" onPress={signOut} />
-    </Container>
+    <SC.Container>
+      <SC.Header>
+        <SC.BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" color="#999591" size={24} />
+        </SC.BackButton>
+        <SC.HeaderTitle>Barbers</SC.HeaderTitle>
+        <SC.UserAvatar source={{ uri: user.avatar_url }} />
+      </SC.Header>
+    </SC.Container>
   );
 };
 
--- src/pages/CreateAppointment/styles.tsx
+++ src/pages/CreateAppointment/styles.tsx
@@ -1,3 +1,39 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  ${Platform.OS === 'ios' &&
+  css`
+    padding-top: ${getStatusBarHeight()}px;
+  `}
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  /*  */
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f5ede8;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  margin-left: 16px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
+`;
