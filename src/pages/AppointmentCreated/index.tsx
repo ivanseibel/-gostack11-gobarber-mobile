cf8d784a1adb6eaaeb7ecf3bd3a2322535e4b7fb
Ivan Seibel <ivanseibel@gmail.com>
2020-07-27 19:21:27 -0300

    Add Create Appointment Success page.

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import * as SC from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
 
   return (
-    <Container style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Logoff" onPress={signOut} />
-    </Container>
+    <SC.Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <SC.Title>Appointment scheduled</SC.Title>
+      <SC.Description>
+        Tuesday, March 14, 2020, at 12 pm, with Ivan Seibel.
+      </SC.Description>
+
+      <SC.OkButton onPress={handleOkPressed}>
+        <SC.OkButtonText>Ok</SC.OkButtonText>
+      </SC.OkButton>
+    </SC.Container>
   );
 };
 
