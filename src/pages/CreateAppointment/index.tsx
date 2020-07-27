ca8eb09931fb22fa50215db12a41f0cf218de0a4
Ivan Seibel <ivanseibel@gmail.com>
2020-07-27 08:43:53 -0300

    Implement provider selection slider.

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,23 +1,45 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { useRoute, useNavigation } from '@react-navigation/native';
 import * as SC from './styles';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 interface IRouteParams {
   providerId: string;
 }
 
+export interface IProvider {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 const CreateAppointment: React.FC = () => {
   const route = useRoute();
-  const { providerId } = route.params as IRouteParams;
+  const routeParams = route.params as IRouteParams;
   const { user } = useAuth();
   const navigation = useNavigation();
+  const [providers, setProviders] = useState<IProvider[]>();
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
 
   const navigateBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const handleSelectProvider = useCallback(providerId => {
+    setSelectedProvider(providerId);
+  }, []);
 
   return (
     <SC.Container>
@@ -25,9 +47,33 @@ const CreateAppointment: React.FC = () => {
         <SC.BackButton onPress={navigateBack}>
           <Icon name="chevron-left" color="#999591" size={24} />
         </SC.BackButton>
+
         <SC.HeaderTitle>Barbers</SC.HeaderTitle>
+
         <SC.UserAvatar source={{ uri: user.avatar_url }} />
       </SC.Header>
+
+      <SC.ProvidersListContainer>
+        <SC.ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <SC.ProviderContainer
+              onPress={() => {
+                handleSelectProvider(provider.id);
+              }}
+              selected={provider.id === selectedProvider}
+            >
+              <SC.ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <SC.ProviderName selected={provider.id === selectedProvider}>
+                {provider.name}
+              </SC.ProviderName>
+            </SC.ProviderContainer>
+          )}
+        />
+      </SC.ProvidersListContainer>
     </SC.Container>
   );
 };
