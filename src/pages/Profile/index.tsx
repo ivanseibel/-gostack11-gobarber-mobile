8d6a3edb1f374db47cb505976166b140ffee5bbb
Ivan Seibel <ivanseibel@gmail.com>
2020-07-28 10:28:28 -0300

    Implement layout.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,17 +1,188 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useRef, useCallback } from 'react';
+import {
+  KeyboardAvoidingView,
+  View,
+  Platform,
+  ScrollView,
+  TextInput,
+  Alert,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/Feather';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import * as SC from './styles';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
-const Profile: React.FC = () => {
-  const { signOut } = useAuth();
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const oldPasswordInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const passwordConfirmationInputRef = useRef<TextInput>(null);
+
+  const navigation = useNavigation();
+  const { user } = useAuth();
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData): Promise<void> => {
+      formRef.current?.setErrors({});
+      try {
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Name is required'),
+          email: Yup.string()
+            .required('Email is required')
+            .email('Must be a valid email'),
+          password: Yup.string().min(6, 'Min 6 characters'),
+        });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('users', {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        });
+
+        navigation.goBack();
+
+        Alert.alert(
+          'Well done',
+          'You are registered on GoBarber and ready to logon.',
+        );
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+
+        Alert.alert('Oh-oh', 'Please, verify your data and try again.');
+      }
+    },
+    [navigation],
+  );
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
 
   return (
-    <Container style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Logoff" onPress={signOut} />
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <SC.Container>
+            <SC.BackButton onPress={handleGoBack}>
+              <Icon name="chevron-left" size={24} color="#999591" />
+            </SC.BackButton>
+
+            <SC.UserAvatarButton onPress={() => {}}>
+              <SC.UserAvatar source={{ uri: user.avatar_url }} />
+            </SC.UserAvatarButton>
+
+            <View>
+              <SC.Title>Profile</SC.Title>
+            </View>
+
+            <Form onSubmit={handleSignUp} ref={formRef}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Name"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  oldPasswordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={oldPasswordInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                name="old_password"
+                icon="lock"
+                placeholder="Old password"
+                returnKeyType="next"
+                containerStyle={{ marginTop: 16 }}
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                name="password"
+                icon="lock"
+                placeholder="New password"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordConfirmationInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordConfirmationInputRef}
+                secureTextEntry
+                textContentType="newPassword"
+                name="password_confirmation"
+                icon="lock"
+                placeholder="New password confirmation"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Update
+              </Button>
+            </Form>
+          </SC.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
-export default Profile;
+export default SignUp;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: flex-start;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 16px 0;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 40px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  /* margin-top: 32px; */
+`;
+
+export const UserAvatar = styled.Image`
+  width: 160px;
+  height: 160px;
+  border-radius: 80px;
+  align-self: center;
+`;
