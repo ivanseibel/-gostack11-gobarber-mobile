85b730450ba61dee2c2ddb0f5d753020030917e4
Ivan Seibel <ivanseibel@gmail.com>
2020-07-28 18:30:54 -0300

    Implement profile update method.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useEffect } from 'react';
 import {
   KeyboardAvoidingView,
   View,
@@ -12,6 +12,7 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
+import ImagePicker from 'react-native-image-picker';
 import getValidationErrors from '../../utils/getValidationErrors';
 import api from '../../services/api';
 
@@ -21,10 +22,12 @@ import Button from '../../components/Button';
 import * as SC from './styles';
 import { useAuth } from '../../hooks/auth';
 
-interface SignUpFormData {
+interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const SignUp: React.FC = () => {
@@ -36,10 +39,10 @@ const SignUp: React.FC = () => {
   const passwordConfirmationInputRef = useRef<TextInput>(null);
 
   const navigation = useNavigation();
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
 
-  const handleSignUp = useCallback(
-    async (data: SignUpFormData): Promise<void> => {
+  const handleProfile = useCallback(
+    async (data: ProfileFormData): Promise<void> => {
       formRef.current?.setErrors({});
       try {
         const schema = Yup.object().shape({
@@ -47,39 +50,72 @@ const SignUp: React.FC = () => {
           email: Yup.string()
             .required('Email is required')
             .email('Must be a valid email'),
-          password: Yup.string().min(6, 'Min 6 characters'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: true,
+            then: Yup.string()
+              .min(6, 'Min 6 characters')
+              .required('Password is required'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password'), undefined],
+            'Confirmation must match',
+          ),
         });
+
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('users', {
-          name: data.name,
-          email: data.email,
-          password: data.password,
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const validatedData = {
+          name,
+          email,
+          ...(old_password
+            ? { old_password, password, password_confirmation }
+            : null),
+        };
+
+        await api.put('profile', validatedData).then(response => {
+          updateUser(response.data);
         });
 
         navigation.goBack();
 
-        Alert.alert(
-          'Well done',
-          'You are registered on GoBarber and ready to logon.',
-        );
+        Alert.alert('Well done', 'Your data was updated');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
         }
 
-        Alert.alert('Oh-oh', 'Please, verify your data and try again.');
+        Alert.alert(
+          'Oh-oh... there is something wrong',
+          'Please, verify your data and try again',
+        );
       }
     },
-    [navigation],
+    [navigation, updateUser],
   );
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  useEffect(() => {
+    formRef.current?.setData({
+      name: user.name,
+      email: user.email,
+    });
+  }, [user]);
 
   return (
     <>
@@ -107,7 +143,7 @@ const SignUp: React.FC = () => {
               <SC.Title>Profile</SC.Title>
             </View>
 
-            <Form onSubmit={handleSignUp} ref={formRef}>
+            <Form onSubmit={handleProfile} ref={formRef}>
               <Input
                 autoCapitalize="words"
                 name="name"
